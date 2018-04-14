c422a660bfe3a66910cbbc65f10b86189e4e00ab
Semen Bevzuk <semen.bevzuk@gmail.com>
2018-04-14 23:43:53 +0300

    Добавил тесты для последовательной версии.

--- groups/1403-3/bevzuk_sa/1-test-version/script.bat
+++ groups/1403-3/bevzuk_sa/1-test-version/script.bat
@@ -1,4 +1,15 @@
+
+@echo 
+echo Size array 
+set /p n=
+ 
+#echo Name of the test file
+#set /p name=
+
 cd Debug
-START "" /WAIT Generator.exe
-START "" /WAIT Task.exe
-START Checker.exe
+START "" /WAIT Generator.exe %n% Test%n%
+START "" /WAIT Task.exe Test%n%
+START "" /WAIT Checker.exe Test%n%
+
+cd ..
+DEL Test%n%.out
